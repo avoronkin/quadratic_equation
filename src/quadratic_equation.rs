1779ec424c32029a7281573c9b4e1533efc70237
alex <admin@elasticweb.ru>
2022-03-19 13:10:59 +0300

    add epsilon

--- src/quadratic_equation.rs
+++ src/quadratic_equation.rs
@@ -4,19 +4,22 @@ use thiserror::Error;
 pub enum QuadraticEquatationError {
     #[error("коэффициент a не может быть равен 0")]
     EqualToZero,
+    #[error("переполнение")]
+    Overflow,
 }
 
 pub fn solve(a: f64, b: f64, c: f64) -> Result<Vec<f64>, QuadraticEquatationError> {
-    if a == 0.0 {
+    if a.abs() < f64::EPSILON {
         return Err(QuadraticEquatationError::EqualToZero);
     }
 
     let mut result: Vec<f64> = vec![];
 
     let discriminant = b * b - 4.0 * a * c;
-    if discriminant == 0.0 {
+
+    if discriminant.abs() < f64::EPSILON {
         result.push((-b + discriminant.sqrt()) / (2.0 * a));
-    } else if discriminant > 0.0 {
+    } else if discriminant >= f64::EPSILON {
         result.push((-b + discriminant.sqrt()) / (2.0 * a));
         result.push((-b - discriminant.sqrt()) / (2.0 * a));
     }
@@ -52,6 +55,13 @@ mod tests {
     #[test]
     fn not_a_quadratic_equation() {
         let err = solve(0.0, 2.0, 1.0).unwrap_err();
+        let expected_err = QuadraticEquatationError::EqualToZero;
+        assert_eq!(err, expected_err);
+    }
+
+    #[test]
+    fn not_a_quadratic_equation_with_a_less_then_epsilon() {
+        let err = solve(2.21e-16f64, 2.0, 1.0).unwrap_err();
         let expected_err = QuadraticEquatationError::EqualToZero;
         assert_eq!(err, expected_err);
     }
